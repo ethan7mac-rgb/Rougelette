e0efc6fc1b4f7994feeb93e0dc988aed2087a246
Ethan MacDonald <ethan7mac@gmail.com>
2026-04-08 11:09:11 -0300

    Made extra money - Jeremy
    ^

---
+++ Rougelette/Items/ExtraMoney.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rougelette.Items
+{
+    public sealed class ExtraMoney : Item
+    {
+        public ExtraMoney()
+        {
+            Name = "Extra Return from Number";
+            Price = 10;
+            Durability = 1;
+        }
+        public int MoneyMoney()
+        {
+            return 1;
+        }
+    }
+}
